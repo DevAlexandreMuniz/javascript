940d4a9c53c8d230c68369053e5e1d13dd236c5d
Alexandre Ribeiro Muniz <95039766+DevAlexandreMuniz@users.noreply.github.com>
2021-12-31 19:22:56 -0400

    criei ex014

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+
+function carregar() {
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.querySelector('img#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia!
+        img.src = 'imagens/manhã.png'
+        document.body.style.background = '#f7c97e'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa tarde!
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#a6adbf'
+    } else {
+        //Boa noie!
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#011243'
+    }
+}
