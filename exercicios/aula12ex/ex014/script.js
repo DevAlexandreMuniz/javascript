7d20b950c32daae07b8e11892023ae0e3d730645
Alexandre Ribeiro Muniz <95039766+DevAlexandreMuniz@users.noreply.github.com>
2021-12-31 20:45:31 -0400

    atualizei ex014

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -5,11 +5,16 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
+
+    if (hora < 6) {
+        //Boa madrugada
+        img.src = 'imagens/madrugada.png'
+        document.body.style.background = '#001118'
+    } else if (hora < 12) {
         //Bom dia!
         img.src = 'imagens/manhã.png'
         document.body.style.background = '#f7c97e'
-    } else if (hora >= 12 && hora <= 18) {
+    } else if (hora <= 18) {
         //Boa tarde!
         img.src = 'imagens/tarde.png'
         document.body.style.background = '#a6adbf'
