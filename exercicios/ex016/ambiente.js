41f5038af73d9812bb26e42077a761f7b6bcc8e5
Alexandre Ribeiro Muniz <95039766+DevAlexandreMuniz@users.noreply.github.com>
2022-01-20 11:00:30 -0400

    criei ex016

---
+++ exercicios/ex016/ambiente.js
@@ -0,0 +1,13 @@
+let num = [6,4,9,2]
+num.push(7)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(7)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado`)
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
---
+++ exercicios/ex016/vetornatela.js
@@ -0,0 +1,24 @@
+let num = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(num)
+
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+/*
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+console.log(num[5])
+console.log(num[6])
+console.log(num[7])
+console.log(num[8])
+console.log(num[9])
+*/
